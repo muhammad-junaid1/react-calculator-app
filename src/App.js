2f29a6aa638f028329460af0e0308b14c9bc235a
muhammad-junaid1 <mjunaid.swe@gmail.com>
2023-01-29 19:57:54 +0500

    validation for calculation value

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ function App() {
   const handleClick = (event) => {
     const buttonValue = event.target.innerText;
     setValue((value) => {
-      if(parseFloat(value)) {
-        return value + buttonValue;
+      if(value.length >= 20) {
+        return value;
       } else {
-        return buttonValue;
+        if(value.includes(".") && buttonValue === ".") {
+          return value; 
+        }
+        if(parseFloat(value)) {
+          return value + buttonValue;
+        } else {
+          return buttonValue;
+        }
       }
     });
   }
   const allClear = () => {
     setValue("");
   }
+
   const deleteChar = () => {
     setValue((value) => {
       return value.slice(0, value.length - 1);
@@ -32,11 +40,11 @@ function App() {
     <>
       <div className="container w-1/4 rounded mt-[80px] mx-auto shadow-md">
           <div className="relative rounded-tl rounded-tr bg-blue-600 text-white py-10 font-bold text-right">
-              <p className="text-[14px] absolute top-2 right-4"></p>
+              <p className="text-[18px] font-light absolute top-2 text-gray-200 right-4">Ans = 0</p>
               <p className="text-[28px] absolute bottom-1 right-4">{value}</p>
           </div>
           <div className="grid grid-cols-4">
-              <button onClick={allClear} className="col-span-2 bg-slate-400 text-white">AC</button>
+              <button onClick={allClear} className="col-span-2 bg-slate-300">AC</button>
               <button onClick={deleteChar} className="p-4 bg-gray-200 border border-lime-50 hover:bg-gray-300 border-collapse">DEL</button>
               <button className="p-4 bg-gray-200 border border-lime-50 hover:bg-gray-300 border-collapse">+</button>
               {[7, 8, 9].map((btn) => <button key={btn} onClick={handleClick} className="p-4 bg-gray-200 border border-lime-50 hover:bg-gray-300 border-collapse">{btn}</button>)}
@@ -45,9 +53,9 @@ function App() {
               <button className="p-4 bg-gray-200 border border-lime-50 hover:bg-gray-300 border-collapse">+</button>
               {[1, 2, 3].map((btn) => <button key={btn} onClick={handleClick} className="p-4 bg-gray-200 border border-lime-50 hover:bg-gray-300 border-collapse">{btn}</button>)}
               <button className="p-4 bg-gray-200 border border-lime-50 hover:bg-gray-300 border-collapse">-</button>
-              <button className="p-4 bg-gray-200 border border-lime-50 hover:bg-gray-300 border-collapse">.</button>
+              <button onClick={handleClick} className="p-4 bg-gray-200 border border-lime-50 hover:bg-gray-300 border-collapse">.</button>
               <button onClick={handleClick} className="p-4 bg-gray-200 border border-lime-50 hover:bg-gray-300 border-collapse">0</button>
-              <button className="col-span-2 bg-slate-400 text-white text-[28px]">=</button>
+              <button className="col-span-2 bg-slate-300 text-[24px]">=</button>
           </div>
       </div>
     </>
